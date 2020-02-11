9df2a1203d735a350d2e2e7c60bdd684c828e7f5
Ethan Bellmer <e.bellmer@outlook.com>
2020-02-11 11:43:26 +0000

    Import DB connector functions

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-#
-#
-#
-#
-#
+# Title: Plotly Dash Dashboard for MOVE
+# Description: Dashboard implementation for the sensors installed 
+# 				in the Slaford Museum and Art Gallery using Plotly's Dash system
+# Author: Ethan Bellmer
+# Date: 06.02.2020
+# Version: 0.1
 
 # Venv activation is blocked by default because the process isn't singed, so run this first:
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
+
+# Library imports
 import os
 import pyodbc
 import traceback 
@@ -18,11 +21,14 @@ import dash_auth
 
 import json 
 
+# Custom functions
+import dbConnect
+
+
 # Temporary user login credentials
 #SQL Server connection info
 with open(".usrCreds") as f:
 	usrCreds = json.load(f)
-
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
