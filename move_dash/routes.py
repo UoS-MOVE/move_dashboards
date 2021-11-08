eb06ddfc910237d42e27cc2d3ffd6fa04f207966
Ethan Bellmer <e.bellmer@outlook.com>
2021-11-08 15:25:06 +0000

    Misc

--- move_dash/routes.py
+++ move_dash/routes.py
@@ -17,8 +17,8 @@ def home():
     """Landing page."""
     return render_template(
         'index.jinja2',
-        title='Plotly Dash Flask Tutorial',
-        description='Embed Plotly Dash into your Flask applications.',
+        title='MOVE Dashboard',
+        description='Prototype for the MOVE Dashboard.',
         template='home-template',
         body="This is a homepage served with Flask."
     )
